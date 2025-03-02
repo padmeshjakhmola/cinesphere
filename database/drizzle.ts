c084fd5e30a15491d5b846a96b1b7b69ea0c4a18
Padmesh Jakhmola <prankshadow@gmail.com>
2025-03-02 15:55:17 +0530

    retryingggg

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL!;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL environment variable is not set");
