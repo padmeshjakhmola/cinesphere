c5de8306b16bf08b297985144980a6e49adc5f30
Padmesh Jakhmola <prankshadow@gmail.com>
2025-03-07 17:46:05 +0530

    feat: added aws delete s3 object

--- lib/actions/delete.ts
+++ lib/actions/delete.ts
@@ -1,10 +1,34 @@
 import { db } from "@/database/drizzle";
 import { movies } from "@/database/schema";
 import { eq } from "drizzle-orm";
+import config from "../config";
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+import s3 from "@/utils/aws";
 
 export const deleteSingleMovie = async (movieId: string) => {
   try {
-    await db.delete(movies).where(eq(movies.id, movieId));
+    const response = await db
+      .delete(movies)
+      .where(eq(movies.id, movieId))
+      .returning({ image: movies.moviePoster, video: movies.movieVideo });
+
+    const imageKey = response[0].image;
+    const videoKey = response[0].video;
+
+    const imageParams = {
+      Bucket: config.env.awsBucketname,
+      Key: imageKey,
+    };
+
+    const videoParams = {
+      Bucket: config.env.awsBucketname,
+      Key: videoKey,
+    };
+
+    await Promise.all([
+      s3.send(new DeleteObjectCommand(imageParams)),
+      s3.send(new DeleteObjectCommand(videoParams)),
+    ]);
 
     return {
       sucess: true,
